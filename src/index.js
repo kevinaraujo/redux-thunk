159296fe6d9f484406017e8c264b3297eb76ffe4
Kevin Araujo <kevinaraujo@Kevin-Araujos-MacBook-Pro-ArcTouch-447A.local>
2023-03-15 13:48:03 -0300

    feat: Added toast component.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import Router from 'routes';
 import './index.css';
 import store from './store';
 import { Provider } from 'react-redux';
+import { createStandaloneToast, Toast } from '@chakra-ui/toast';
+
+const { ToastContainer, toast } = createStandaloneToast();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
       <Router />
+      <ToastContainer />
     </Provider>
 );
+
+toast({
+  description: 'OK',
+  duration: 2000
+});
