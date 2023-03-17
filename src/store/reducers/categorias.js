04fba29d9e68664342a97a6214debd44b0fcfebb
Kevin Araujo <kevinaraujo@Kevin-Araujo-C02FJ852MD6N.local>
2023-03-17 16:40:37 -0300

    Added addCase using 3 status on categorias reducer file.

--- src/store/reducers/categorias.js
+++ src/store/reducers/categorias.js
@@ -1,6 +1,9 @@
+import { createStandaloneToast } from '@chakra-ui/toast';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import categoriasService from 'services/categorias';
+import busca from './busca';
 
+const { toast } = createStandaloneToast();
 const initialState = [];
 
 export const buscarCategorias = createAsyncThunk(
@@ -13,10 +16,45 @@ const categoriasSlice = createSlice({
   initialState,
   extraReducers: builder => {
     console.log('builder', builder)
-    builder.addCase(buscarCategorias.fulfilled, (state, { payload }) => {
+    builder
+    .addCase(
+      buscarCategorias.fulfilled,
+      (state, { payload }) => {
+        toast({
+          title: 'Success',
+          description: 'Categorias loaded!',
+          status: 'success',
+          duration: 2000,
+          isClosable: true
+        })
       
-      return payload
-    })
+        return payload
+      }
+    )
+    .addCase(
+      buscarCategorias.pending,
+      (state, { payload }) => {
+        toast({
+          description: 'Loading itens...',
+          status: 'loading',
+          duration: 2000,
+          isClosable: true
+        });
+       }
+    )
+    .addCase(
+      buscarCategorias.rejected,
+      (state, { payload }) => {
+        toast({
+          title: 'Error',
+          description: 'Error on loading categorias',
+          status: 'error',
+          duration: 2000,
+          isClosable: true
+        })
+      }
+    )
+    
   }
 });
 
